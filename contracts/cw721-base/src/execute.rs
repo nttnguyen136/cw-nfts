c4a03aa8ee771f62c97d10b722440b27358cdcbf
nttnguyen136 <nttnguyen136@gmail.com>
2022-08-31 08:54:17 +0700

    update instantiate

--- contracts/cw721-base/src/execute.rs
+++ contracts/cw721-base/src/execute.rs
@@ -38,7 +38,7 @@ where
         let minter = deps.api.addr_validate(&msg.minter)?;
         self.minter.save(deps.storage, &minter)?;
         Ok(Response::new()
-                    .add_attribute("instantiate","10"))
+                    .add_attribute("instantiate","11"))
     }
 
     pub fn execute(
