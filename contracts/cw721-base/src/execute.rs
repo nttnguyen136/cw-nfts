ab58d4ecde401fe9bfbf5490edcee744fa4fb11b
nttnguyen136 <nttnguyen136@gmail.com>
2023-03-02 09:14:20 +0700

    update code 84

--- contracts/cw721-base/src/execute.rs
+++ contracts/cw721-base/src/execute.rs
@@ -38,7 +38,7 @@ where
         let minter = deps.api.addr_validate(&msg.minter)?;
         self.minter.save(deps.storage, &minter)?;
         Ok(Response::new()
-                    .add_attribute("instantiate","83"))
+                    .add_attribute("instantiate","84"))
     }
 
     pub fn execute(
